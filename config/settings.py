208adf68d2f8d6f33706f4ce1be0ef2476ce42e2
Pratham <nxtwave@Prathams-MacBook-Pro.local>
2026-06-22 10:52:56 +0530

    Initial empty commit

--- config/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-MODEL_NAME = "gpt-5.4-mini-2026-03-17"
--- testing.py
+++
@@ -1,5 +0,0 @@
-from utils.rag import search_kb, format_context
-
-results = search_kb("What is My Journey?")
-print(format_context(results))
-
